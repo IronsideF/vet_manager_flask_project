edea34300e9f18a8948ea6da03a35c7a2e173523
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-07-14 15:29:42 +0100

    built basic classes

--- db/vet_manager.sql
+++ db/vet_manager.sql
@@ -6,13 +6,14 @@ CREATE TABLE vets (
     first_name VARCHAR,
     last_name VARCHAR,
     specialism VARCHAR
-)
+);
 
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
     name VARCHAR,
     dob VARCHAR,
+    type VARCHAR,
     owner_details VARCHAR,
     treatment_notes VARCHAR,
     vet_id INT NOT NULL REFERENCES vets(id) ON DELETE CASCADE
-)
+);
--- models/vet.py
+++ models/vet.py
@@ -1,2 +1,6 @@
 class Vet:
-    pass
+    def __init__(self, _f_name, _l_name, _specialism, _id=None):
+        self.first_name=_f_name
+        self.last_name=_l_name
+        self.specialism=_specialism
+        self.id=_id
