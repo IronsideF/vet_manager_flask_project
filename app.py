0154247f107b26278de12437a1ed3a1292785092
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-07-14 15:50:29 +0100

    added gitignore and set up hello world html

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-from controllers import controller
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
